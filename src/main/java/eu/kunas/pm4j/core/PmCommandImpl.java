a75307b0f8bb2d931e1194651c46a526715eeb30
Ramazan Kunas <ramazan@kunas.eu>
2015-07-03 14:43:09 +0200

    Moving

--- src/main/java/eu/kunas/pm4j/core/PmCommandImpl.java
+++ src/main/java/eu/kunas/pm4j/core/PmCommandImpl.java
@@ -11,27 +11,9 @@ import java.io.Serializable;
 public abstract class PmCommandImpl<T_PM_PARENT> extends PmImpl<T_PM_PARENT> implements Serializable {
 
     private static Log log = LogFactory.getLog(PmCommandImpl.class);
-    private String onComplete = "";
-    private String render = "";
 
     public PmCommandImpl(final T_PM_PARENT parent) {
         super(parent);
-    }
-
-    public String getRender() {
-        return render;
-    }
-
-    public void setRender(final String render) {
-        this.render = render;
-    }
-
-    public String getOnComplete() {
-        return onComplete;
-    }
-
-    public void setOnComplete(final String onComplete) {
-        this.onComplete = onComplete;
     }
 
     public final void doIt() {
