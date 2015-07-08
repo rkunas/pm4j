6aef7607afa30efa6562942d165da5de40b77c1e
Ramazan <ramazan@kunas.eu>
2015-07-08 08:05:03 +0200

    FXCombobox

--- src/main/java/eu/kunas/pm4j/fx/FXComboBox.java
+++ src/main/java/eu/kunas/pm4j/fx/FXComboBox.java
@@ -6,6 +6,7 @@ import eu.kunas.pm4j.fx.base.FXControl;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.util.StringConverter;
 
 /**
  * Created by Kunas on 07.07.2015.
@@ -20,6 +21,34 @@ public class FXComboBox extends FXControl<PmAttrImpl> {
 
     @Override
     public void refresh() {
+
+        comboBox.setConverter(stringConverter);
+        comboBox.getItems().clear();
+        comboBox.valueProperty().set(null);
+        comboBox.getItems().addAll(pm.getOptionSet().getOptions());
+        if (pm.getValue() != null) {
+            comboBox.getSelectionModel().clearSelection();
+            String enumerable = (String) pm.getValue();
+            comboBox.getSelectionModel().select(pm.getOptionSet().findOptionForIdString(enumerable));
+        } else {
+            comboBox.getSelectionModel().clearSelection();
+        }
+
         label.setText(pm.getTitle());
     }
+
+    private final StringConverter stringConverter = new StringConverter() {
+        @Override
+        public String toString(final Object t) {
+            PmOption option = (PmOption) t;
+            return ((PmOption) t).getPmTitle();
+        }
+
+        @Override
+        public Object fromString(final String string) {
+
+            return null;
+
+        }
+    };
 }
