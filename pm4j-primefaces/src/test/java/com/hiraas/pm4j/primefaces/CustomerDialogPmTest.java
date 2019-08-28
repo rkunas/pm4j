f9760c2b8ef1bd33d2d1ac5259552fcfd675bac6
rkunas <rkunas@atrify.com>
2019-08-28 12:19:05 +0200

    PM4J with Primefaces

--- pm4j-primefaces/src/test/java/com/hiraas/pm4j/primefaces/CustomerDialogPmTest.java
+++ pm4j-primefaces/src/test/java/com/hiraas/pm4j/primefaces/CustomerDialogPmTest.java
@@ -1,4 +1,12 @@
 package com.hiraas.pm4j.primefaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CustomerDialogPmTest {
+
+    @Test
+    public void testSome(){
+        Assert.assertTrue(true);
+    }
 }
